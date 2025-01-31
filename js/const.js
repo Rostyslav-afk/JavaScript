5146ce1fe48fc8cb1c710c3e0f298357e7954ff0
Rostyslav-afk <110056857+Rostyslav-afk@users.noreply.github.com>
2025-01-31 20:09:49 +0100

    full

---
+++ js/const.js
@@ -0,0 +1,35 @@
+"use strict";
+const userName = "veazzerr";
+console.log(userName.length);
+console.log(typeof (userName));
+const number = 5;
+console.log(typeof (number));
+const IsStudent = true;
+console.log(typeof (IsStudent));
+const IsActive = false;
+console.log(typeof (IsActive))
+const basket = null;
+console.log(typeof (basket))
+let totalCount = "10";
+console.log(typeof (totalCount))
+// Всього є 7 типів:
+// string , number , bigint , boolean , symbol , null,  undefined
+
+
+//НЕ МОЖНА ВИКОРИСТОВУВАТИ ТАКІ СЛОВА ДО КЛАСІВ
+// abstract	arguments	await	boolean
+// break	byte	case	catch
+// char	class	const	continue
+// debugger	default	delete	do
+// double	else	enum	eval
+// export	extends	false	final
+// finally	float	for	function
+// goto	if	implements	import
+// in	instanceof	int	interface
+// let	long	native	new
+// null	package	private	protected
+// public	return	short	static
+// super	switch	synchronized	this
+// throw	throws	transient	true
+// try	typeof	var	void
+// volatile	while	with	yield
