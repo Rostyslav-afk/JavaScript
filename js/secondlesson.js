d9acb7436215f5ea1c6d1ece49f8e54e87799848
Rostyslav-afk <110056857+Rostyslav-afk@users.noreply.github.com>
2025-02-03 19:34:34 +0100

    without-threelesson

---
+++ js/secondlesson.js
@@ -0,0 +1,19 @@
+// const lower = "LOWER CASE";
+// console.log(lower.toLowerCase());
+// const upper = "upper";
+// console.log(upper.toUpperCase());
+
+// const goIteens = "I'm stydying in GoIteens(original length 44)";
+// console.log(goIteens.length -1);
+
+// alert("Hi")
+
+// const hi = "Like?";
+// confirm(hi);
+
+// const question = "How old are you?"
+// prompt(question)
+
+
+
+
---
+++ js/terceroleccion.js
@@ -0,0 +1,7 @@
+const x = 10;
+const y = 5;
+
+// Додавання 
+console.log(x + y); //15
+
+
