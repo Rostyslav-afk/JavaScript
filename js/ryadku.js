eea2cddef5e23234848c1bdb8fcc8c9242de3a73
Rostyslav-afk <110056857+Rostyslav-afk@users.noreply.github.com>
2025-02-14 20:18:04 +0100

    endLesson

---
+++ js/ryadku.js
@@ -0,0 +1,41 @@
+// const message = "I love the programming language Java Script";
+// console.log(typeof(message));
+// const messageLength = message.length;
+// console.log(messageLength);
+// const firstLetterMessage = message[0];
+// console.log(firstLetterMessage);
+// const lastLetterMessage = message[messageLength -4];
+// console.log(lastLetterMessage);
+
+// const age = 12;
+// const message = "My age is";
+// const result = message + " " + age;
+// console.log(result);
+// console.log(typeof(result));
+// const x = "5";
+// const y = 5;
+// const c = 20;
+// const res = c + y + x;
+// console.log(res);
+
+// const message = "I love the programming language";
+// const messageJavaScript = "Java Script";
+// const fullMessage = message + " " + messageJavaScript;
+// const result = `${message} hola ${messageJavaScript}`;
+// console.log(result);
+
+// const namee = "Rostyslav";
+// const subname = "Skrypyn";
+// const result = namee + " " + subname;
+// const length = result.length;
+// const shablon = `Мене звати ${result} Кількість символів у рядку ${length}`
+// console.log(shablon);
+
+// Задача 2
+// Напишіть скрипт, який знаходить площу прямокутника. Висотою 23см, шириною 10см. Всі данні повинні зберігатися у змінних. Значення площі повинно зберігатися у числовій змінній, результат вивести у консоль шаблонним рядком.
+// const height = 23;
+// const width = 10;
+// const rectangleArea = height * width;
+// const answer = `Площа прямокутника: ${rectangleArea} сантиметрів квадратних`;
+// console.log(answer);
+
