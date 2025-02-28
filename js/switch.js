bca87a6b172590ce434e36d8528a65eb55818916
Rostyslav-afk <110056857+Rostyslav-afk@users.noreply.github.com>
2025-02-28 20:03:44 +0100

    s

---
+++ js/switch.js
@@ -0,0 +1,33 @@
+// const age = 21;
+// const type = (age >= 18) ? "parent": "child";
+// console.log(type);
+
+
+// пошук найбільшого числа
+// створіть зміну num1 і призначте їй 5
+// створіть зміну num2 і призначте їй 10
+// перевірте яке з цих чисел більше і призначте зміній bigNumber num1 або num2
+
+// const num1 = 5;
+// const num2 = 10;
+// let biggerNumber;
+
+// if (num1 > num2) {
+//     biggerNumber = num1;
+// } else {
+//     biggerNumber = num2;
+// }
+// console.log(biggerNumber);
+
+// const a = 2;
+// const b = 3;
+// const result = (a + b > 4) ? "Вище": "Нижче";
+// console.log(result);
+
+
+//Напишіть код що отримує число з input та:
+// якщо число більше 1 виведіть повідомлення число більше 1 та виведіть число через шаблоний рядок на екран, якщо менше то  виведіть повідомлення число менше 1 та виведіть число через шаблоний рядок на екран.
+
+// const whatNumber = Number(prompt("Введіть число"));
+// const result = (whatNumber > 1) ? `Так ${whatNumber} більше за один`: `Ні число ${whatNumber} не більше за один`;
+// console.log(result);
