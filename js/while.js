fe215a6cb39a972df0912bae14273af0c6f8349e
Rostyslav-afk <110056857+Rostyslav-afk@users.noreply.github.com>
2025-03-07 19:39:57 +0100

    wtf

---
+++ js/while.js
@@ -0,0 +1,47 @@
+//СТРУКТУРА FOR
+// for (initialization; condition; post-expression; ) {
+//     // statements
+// }
+// for (let i = 0; i < 10; i += 1) {
+//     console.log(i);
+// }
+// const message = "I love JavaScript!";
+// // const result = message[message.length - 1];
+// // console.log(result);
+// for (let i = 0; i < message.length - 1; i += 1) {
+//     console.log(message[i]);
+    
+// }
+
+//напиши скрипт, який підрахує загальну зарплату працівників
+// кількість працівників зберігається у зміній emplois
+// зарплата кожного працівник, це випадкове число від 500 до 5000
+// записати загальну сумму в зміну totalSalary та вивести у консоль.
+//
+
+//  let totalSalary = 0;
+//  const employers = 10;
+//  const minSalary = 500;
+//  const maxSalary = 5000;
+
+//  for (let i = 0; i <= employers; i += 1 ){
+//     let result = Math.round(Math.random() * (maxSalary - minSalary) + minSalary);
+//     totalSalary += result;
+//     console.log(`Зарплата працівника ${result} $ `);
+    
+//  }
+
+//  console.log(`Загальна сума всіх працівників ${totalSalary} $`);
+
+// напиши скрипт який підрахує сумму всіх парних чисел, які входять в діапазон між 0 та 50
+// for (let i = 2; i <= 20; i += 2) {
+//     console.log(`Парні числа ${i}`);
+    
+// }
+
+ for (let i = 2; i <= 20; i += 2) {
+     if (i % 2 !== 0) {
+        continue;
+     }
+    console.log(i);  
+ }
