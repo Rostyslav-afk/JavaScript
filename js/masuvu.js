d1138e749011d883c2f958dec6caf5cc8629dbbd
Rostyslav-afk <110056857+Rostyslav-afk@users.noreply.github.com>
2025-03-14 19:20:06 +0100

    masuvu

---
+++ js/masuvu.js
@@ -0,0 +1,14 @@
+// масиви - array
+
+const arr = ["Rostyslav", "Vlad", "Mukola", "Lev"]
+console.log(arr.length -1);
+console.log(arr[arr.length -1]);
+arr[4] = "Юрій Горбунов";
+arr[5] = "Vlad Draga";
+console.log(arr);
+
+// const user = arr[0];
+// console.log(user);
+// arr[0] = "Юра"
+// console.log(arr);
+// console.log(typeof(arr));
