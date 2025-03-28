2018c1359e9e7ebaba82ef0cb5abf07936b55302
Rostyslav-afk <110056857+Rostyslav-afk@users.noreply.github.com>
2025-03-28 20:12:03 +0100

    firstLesson

---
+++ js/functions.js
@@ -0,0 +1,40 @@
+// // Function declaration 
+// // function add() {
+// //     // Тіло функції
+// //     console.log("Привіт,Ростислав");
+// //     return 
+// // }
+// // add()
+
+// // Функціональний вираз
+// // const addNumbers = function () {
+
+// // }
+
+// // // Функціональний вираз - стрілкова функція
+
+// // const addNumberss =  () => {
+
+// // }
+
+// function addtwoNumbers(a, b) {
+//     console.log(a + b);
+// }
+// addtwoNumbers(2, 7)
+// addtwoNumbers(5, 10)
+// addtwoNumbers("Hello", "Hi")
+// addtwoNumbers("When", "undefined")
+// function userPerson(name, age) {
+//     name = prompt("Яке ваше ім'я?")
+//     age = Number(prompt("Скільки вам років?"))
+//     const message = `Мене звати ${name} та мені ${age} років`
+//     return console.log(message);
+// }
+// userPerson()
+
+
+function convertedText (text) {
+    console.log(text.toUpperCase());
+    return text.toUpperCase()
+}
+convertedText("Lorem ")
