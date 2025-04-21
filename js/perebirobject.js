da9ef881a7380e8e60d1e518ed9f75710bc8ea86
Rostyslav-afk <110056857+Rostyslav-afk@users.noreply.github.com>
2025-04-21 17:34:02 +0200

    start

---
+++ js/perebirobject.js
@@ -0,0 +1,10 @@
+const user = {
+    name: "John",
+    age: 30,    
+    friends: ["Mike", "Anna"],
+    location: {
+        country: "Ukraine",
+        city: "Kyiv",
+        address: "Shevchenka 1",
+    },
+}
