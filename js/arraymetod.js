fa9e5575669531f608622e27465321031b867afa
Rostyslav-afk <110056857+Rostyslav-afk@users.noreply.github.com>
2025-05-16 20:06:49 +0200

    full

--- js/arraymetod.js
+++ js/arraymetod.js
@@ -4,7 +4,7 @@ const friends = ["Rostyslav", "Vlad", "Mukola", "Vlad Draga", "Lev"]
 //     console.log(i);
 // }
 
-//Цей метод forEach() перебирає масив, але нічого не повертає 
+//Цей метод forEach() перебирає масив, але нічого не повертає
 // friends.forEach((friend, idx, arr) => {
 //     console.log(`Друг ${friend} під індексом ${idx} в масиві ${arr}`);
 // });
@@ -58,48 +58,48 @@ const friends = ["Rostyslav", "Vlad", "Mukola", "Vlad Draga", "Lev"]
 // });
 
 
-const pets = [
-    {
-        id: "1",
-        name: "Барсик",
-        type: "кіт",
-        color: "сірий",
-        age: 2,
-        weight: 4.5,
-        isAdopted: true,
-        favoriteFoods: ["риба", "молоко"],
-    },
-    {
-        id: "2",
-        name: "Бім",
-        type: "пес",
-        color: "чорний",
-        age: 5,
-        weight: 12,
-        isAdopted: false,
-        favoriteFoods: ["кістка", "м’ясо"],
-    },
-    {
-        id: "3",
-        name: "Кузя",
-        type: "хом’як",
-        color: "білий",
-        age: 1,
-        weight: 0.2,
-        isAdopted: true,
-        favoriteFoods: ["зерно", "морква"],
-    },
-    {
-        id: "4",
-        name: "Лола",
-        type: "папуга",
-        color: "зелений",
-        age: 3,
-        weight: 0.3,
-        isAdopted: false,
-        favoriteFoods: ["яблуко", "зерно"],
-    },
-];
+// const pets = [
+//     {
+//         id: "1",
+//         name: "Барсик",
+//         type: "кіт",
+//         color: "сірий",
+//         age: 2,
+//         weight: 4.5,
+//         isAdopted: true,
+//         favoriteFoods: ["риба", "молоко"],
+//     },
+//     {
+//         id: "2",
+//         name: "Бім",
+//         type: "пес",
+//         color: "чорний",
+//         age: 5,
+//         weight: 12,
+//         isAdopted: false,
+//         favoriteFoods: ["кістка", "м’ясо"],
+//     },
+//     {
+//         id: "3",
+//         name: "Кузя",
+//         type: "хом’як",
+//         color: "білий",
+//         age: 1,
+//         weight: 0.2,
+//         isAdopted: true,
+//         favoriteFoods: ["зерно", "морква"],
+//     },
+//     {
+//         id: "4",
+//         name: "Лола",
+//         type: "папуга",
+//         color: "зелений",
+//         age: 3,
+//         weight: 0.3,
+//         isAdopted: false,
+//         favoriteFoods: ["яблуко", "зерно"],
+//     },
+// ];
 
 // Завдання 1:
 // Отримати масив усіх імен об'єктів (ключ name)
@@ -124,6 +124,57 @@ const pets = [
 // console.log(getPetsByType(pets, "кіт"));
 
 // Отримати імена всіх тварин, які вже знайдені в родину (isAdopted === true).
-const getAdoptedPetNames = (pets) => pets.filter((pet) => pet.isAdopted);
-console.log(getAdoptedPetNames(pets));
+// const getAdoptedPetNames = (pets) => pets.filter((pet) => pet.isAdopted);
+// console.log(getAdoptedPetNames(pets));
 
+
+// const sortedPetsArray = (pets) => pets.sort((a, b) => b.weight - a.weight); //сортує від найбільного до найменшого.Сортування, якщо значенням ключа є число.
+
+// console.log(sortedPetsArray(pets));
+
+// const sortedPetsName = (pets) => pets.sort((a, b) => a.name.localeCompare(b.name));
+
+// console.log(sortedPetsName(pets));
+
+// const reduceArray = (pets) => pets.reduce((acc, value) => acc += value.weight, 0);
+// console.log(reduceArray(pets));
+
+
+// const addWeight = (pets) => {
+//     let sum = 0;
+//     pets.forEach((pet) => {
+//         console.log(pet.weight);
+//         sum += pet.weight;
+//     })
+//     return sum;
+// };
+// console.log(addWeight(pets));
+const tweets = [
+    { id: "000", likes: 5, tags: ["js", "nodejs"] },
+    { id: "001", likes: 2, tags: ["html", "css"] },
+    { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
+    { id: "003", likes: 8, tags: ["css", "react"] },
+    { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
+];
+
+// const reduceTweets = (tweets) => tweets.reduce((acc, value) => {
+//     acc.push(value.tags);
+//     return acc
+// }, []);
+// console.log(reduceTweets(tweets));
+
+// const flateTweets = (tweets) => tweets.flatMap(value => value.tags);
+// console.log(flateTweets(tweets));
+
+const reduceTags = (tweets) => tweets.reduce((acc, value) => {
+    //console.log(acc); порожній масив
+    //console.log(value); обєкт
+    value.tags.forEach(tag => { // по ключу валюе тег перебираємо масив
+        // console.log(tag); отримуєом кожен елемент масиву
+        if (!acc.includes(tag)) { // якшо порожньому масиву немає нашого асс нема елемента то додай 
+            acc.push(tag)
+        }
+    });
+    return acc;
+}, []);
+console.log(reduceTags(tweets));
